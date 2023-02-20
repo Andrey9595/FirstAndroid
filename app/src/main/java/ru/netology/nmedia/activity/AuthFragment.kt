136e94d41b7441997d55f5434e6cb18684b7ad6a
Andrey9595 <anb95.95@mail.ru>
2023-02-20 19:59:10 +0300

    add error handling

--- app/src/main/java/ru/netology/nmedia/activity/AuthFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/AuthFragment.kt
@@ -7,13 +7,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import ru.netology.nmedia.databinding.FragmentAuthBinding
 import ru.netology.nmedia.utils.Utils
 import ru.netology.nmedia.viewmodel.AuthViewModel
+import ru.netology.nmedia.viewmodel.RegistrationViewModel
 
-class AuthFragment: Fragment() {
+class AuthFragment : Fragment() {
 
-    private val viewModel: AuthViewModel by viewModels(ownerProducer = ::requireParentFragment)
+    private val viewModel: RegistrationViewModel by viewModels(ownerProducer = ::requireParentFragment)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,11 +28,26 @@ class AuthFragment: Fragment() {
             false
         )
 
-        binding.enterBtnView.setOnClickListener{
-            val  login = binding.loginView.text.trim().toString()
-            val password = binding.passwordView.text.trim().toString()
-            viewModel.authentication(login, password)
-            findNavController().navigateUp()
+        binding.enterBtnView.setOnClickListener {
+//            val  login = binding.loginView.text.trim().toString()
+//            val password = binding.passwordView.text.trim().toString()
+//            viewModel.authentication(login, password)
+//            findNavController().navigateUp()
+            if (binding.loginView.text.isNotEmpty() &&
+                binding.passwordView.text.isNotEmpty()
+            ) {
+                val login = binding.loginView.text.trim().toString()
+                val password = binding.passwordView.text.trim().toString()
+                viewModel.registerUser(login, password)
+
+            } else Snackbar.make(binding.root, "Заполните все поля", Snackbar.LENGTH_LONG).show()
+        }
+        viewModel.tokenReceived.observe(viewLifecycleOwner) {
+            if (it == 0){
+                findNavController().navigateUp()
+            } else {
+                Snackbar.make(binding.root, "Неверный пароль или логин", Snackbar.LENGTH_LONG).show()
+            }
         }
         return binding.root
     }
